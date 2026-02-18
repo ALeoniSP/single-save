ef9fae850aa46a6cc84af95038d3aed53bc4a327
ALeoniSP <alessandro.leoni.sp@gmail.com>
2026-02-18 15:44:08 +0100

    Add WebFlux skeleton and health endpoint

---
+++ src/main/java/com/aleonisp/singlesave/SingleSaveApplication.java
@@ -0,0 +1,13 @@
+package com.aleonisp.singlesave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSaveApplication.class, args);
+	}
+
+}
